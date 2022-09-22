44d165f57b2a6758323f74ef4436f8a8e3e9cd7b
rofeeqshittu <rofeeqshittu21@gmail.com>
2022-09-22 06:42:19 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,4 +1,4 @@
-#include"main.h"
+#include "main.h"
 
 /**
  * print_most_numbers -  prints the numbers, from 0 to 9, except 2 & 4, followed by a new line.
@@ -12,7 +12,7 @@ void print_most_numbers(void)
 	{
 		if (i == 2 || i == 4)
 			continue;
-		_putchhar('0' + i);
+		_putchar('0' + i);
 	}
 	_putchar('\n');
 }

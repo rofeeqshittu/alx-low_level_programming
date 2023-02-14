6e9cab751883bece5f27530394617a6ca35400ee
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-02-14 23:05:50 +0100

    Getting ready

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * followed by a new line
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,6 @@
+0x08. C - Recursion
+
+-- Recursion Projects
+-- how to implement
+-- situations to implement recursion
+-- situation not to implement recursion

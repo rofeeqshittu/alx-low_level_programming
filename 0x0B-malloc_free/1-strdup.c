f776dfa1bfdcc3751c8ec218052ab48aafe1cca4
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-04-29 18:45:36 +0100

    Second task debugged

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,7 +4,7 @@
 /**
  * _strdup - a function that returns a pointer to a newly allocated space in
  * memory, which contains a copy of the string given as a parameter.
- *@str - string given
+ *@str: string given
  *
  * Return: NULL if str = NULL
  */

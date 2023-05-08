ebf899e81ad1f114d26eb0ebd34e4de1b4565990
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-08 17:28:01 +0100

    some files rechecked

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -7,12 +7,13 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);

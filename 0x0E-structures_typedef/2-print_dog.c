212a038a227050d207b50cc7d47258cb7fa2a68b
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-08 23:30:59 +0100

    2-print_dog.c file now made to print 'nil' if any of the structure member is NULL

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,8 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
+	if (d->name == NULL || d->age < 0 || d->owner == NULL)
+		printf("nil\n");
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);

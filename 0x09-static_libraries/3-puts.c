20c606c892b571dd59a63a2b82b25a112515ac49
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-09 22:59:23 +0100

    now betty compliant

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
-int _puts(const char *str) {
-    int num_chars = 0;
-    while (*str) {
-        putchar(*str++);
-        num_chars++;
-    }
-    putchar('\n');
-    num_chars++;
-    return num_chars;
+/**
+ * _puts - a puts function
+ * @str: a string
+ *
+ * Return: num_chars
+ */
+int _puts(const char *str)
+{
+	int num_chars = 0;
+
+	while (*str)
+	{
+		putchar(*str++);
+		num_chars++;
+	}
+	putchar('\n');
+	num_chars++;
+
+	return (num_chars);
 }

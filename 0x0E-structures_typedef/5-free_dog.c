21318cb17d5f0e2314ec8408cf9458dd8948d70c
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-09 01:20:56 +0100

    last task added, finally

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the memory allocated for a dog
+ * @d: pointer to the dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}

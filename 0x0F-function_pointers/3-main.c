8031094b463de54613d03b3fb60651ff70d5ae30
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-11 00:02:06 +0100

    3-main.c file now betty complaint

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,17 +16,17 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
 	if ((argv[2][0] != '+') && (argv[2][0] != '-') && (argv[2][0] != '-'))
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && (argv[3] == 0))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 
 	result = (get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));

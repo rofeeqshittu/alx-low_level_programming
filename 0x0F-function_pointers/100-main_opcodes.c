aa4fcfb5217a9d6c049e8032127f3734f279e8af
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-05-13 16:45:12 +0100

    understood diff bet. return & exit functions and used accordingly

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(1);
+		return (1);
 	}
 
 	num_bytes = atoi(argv[1]);
 	if (num_bytes < 0)
 	{
 		printf("Error\n");
-		exit(2);
+		return (2);
 	}
 
 	char *opcode = (char *)main;

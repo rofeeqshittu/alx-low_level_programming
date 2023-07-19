d427945def8b1560289abdbda8e19020b90d0eed
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-07-19 18:44:50 +0100

    First task done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints its name
+ * 
+ * Return: always (0)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+
+	return (0);
+}

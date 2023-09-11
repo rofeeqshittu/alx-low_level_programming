6de193daf4228146fe0ce6dcc463d3ae48af741c
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-09-11 21:58:36 +0100

    C - Preprocessor task uploaded

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define FILENAME __FILE__
+
+/**
+ * main - Program to print the name of the source code file
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	printf(FILENAME);
+	printf("\n");
+
+	return (0);
+}

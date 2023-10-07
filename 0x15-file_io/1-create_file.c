1570ed8aeaf951e0b7ca6cf5dbcdd580f56ea905
rofeeqshittu <rofeeqshittu21@gmail.com>
2023-10-07 23:06:35 +0100

    Second p

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of the file
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success,
+ *	-1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+
+	return (1);
+}
